7860307918ec18afecb10775ad4d65a9134a6e50
ArTim <3a932118@gm.student.ncut.edu.tw>
2022-11-01 21:19:50 +0800

    '建立Controller'

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/about', [AboutController::class, 'index'])->name('about.index');
+Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home.index');
+
+Route::get('/about', function () {
+    return view('welcome');
+})->name('about.index');
+
+Route::get('/news', function () {
+    return view('welcome');
+})->name('news.index');
